291e14845649e90df788b7632f21bdf97cdba7ec
bageus <36338626+bageus@users.noreply.github.com>
2026-06-20 00:51:38 +0200

    feat: route all spawned enemies through movement resolver

--- scripts/boarding/boarding_spawn_director.gd
+++ scripts/boarding/boarding_spawn_director.gd
@@ -5,6 +5,7 @@ extends Node
 @export_node_path("PlatformController") var platform_path: NodePath
 @export_node_path("AnchorPathRegistry") var path_registry_path: NodePath
 @export_node_path("BoardingEnemyRegistry") var enemy_registry_path: NodePath
+@export_node_path("BoardingMovementResolver") var movement_resolver_path: NodePath
 @export_node_path("CrewManager") var crew_manager_path: NodePath
 @export_node_path("GroundOrbRegistry") var orb_registry_path: NodePath
 @export var enemy_container_path: NodePath
@@ -18,6 +19,9 @@ var _rng: RandomNumberGenerator = RandomNumberGenerator.new()
 @onready var _platform: PlatformController = get_node(platform_path)
 @onready var _paths: AnchorPathRegistry = get_node(path_registry_path)
 @onready var _registry: BoardingEnemyRegistry = get_node(enemy_registry_path)
+@onready var _movement_resolver: BoardingMovementResolver = get_node(
+	movement_resolver_path
+)
 @onready var _crew: CrewManager = get_node(crew_manager_path)
 @onready var _orbs: GroundOrbRegistry = get_node(orb_registry_path)
 @onready var _container: Node2D = get_node(enemy_container_path)
@@ -73,11 +77,15 @@ func _spawn_enemy(side: int) -> BoardingEnemy:
 		_platform,
 		_paths,
 		_crew,
-		_orbs
+		_orbs,
+		_movement_resolver
+	)
+	var preferred_x: float = (
+		_platform.global_position.x
+		+ float(side) * balance.spawn_distance_from_platform
 	)
 	enemy.global_position = Vector2(
-		_platform.global_position.x
-			+ float(side) * balance.spawn_distance_from_platform,
+		_movement_resolver.find_ground_spawn_x(enemy, preferred_x, side),
 		_orbs.catalog.ground_y - balance.ground_vertical_offset
 	)
 	return enemy
