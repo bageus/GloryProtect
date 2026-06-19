d0548bf22db109474059756db84193aeaca4c294
bageus <36338626+bageus@users.noreply.github.com>
2026-06-19 22:16:41 +0200

    feat: add crew assignment runtime model

---
+++ scripts/crew/crew_assignment_runtime.gd
@@ -0,0 +1,18 @@
+class_name CrewAssignmentRuntime
+extends RefCounted
+
+enum State {
+	ACTIVE,
+	WAITING_FOR_ACTION,
+	MOVING,
+	DEAD,
+}
+
+var defender_id: int
+var current_role: int = CrewRole.Id.FREE_FIGHTER
+var target_role: int = CrewRole.Id.FREE_FIGHTER
+var state: int = State.ACTIVE
+
+
+func _init(new_defender_id: int) -> void:
+	defender_id = new_defender_id
