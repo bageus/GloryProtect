1f830fca4d97d6b6553c166577f3b27274181ea0
bageus <36338626+bageus@users.noreply.github.com>
2026-06-19 22:19:01 +0200

    feat: show crew roles and selected defender in HUD

--- scripts/ui/prototype_hud.gd
+++ scripts/ui/prototype_hud.gd
@@ -5,6 +5,8 @@ extends Control
 @export_node_path("WindSystem") var wind_system_path: NodePath
 @export_node_path("PlatformController") var platform_path: NodePath
 @export_node_path("AnchorSystem") var anchor_system_path: NodePath
+@export_node_path("CrewRoleManager") var crew_role_manager_path: NodePath
+@export_node_path("CrewDebugInput") var crew_debug_input_path: NodePath
 @export_node_path("PrototypeWorld") var world_path: NodePath
 @export_node_path("PrototypeShieldSystem") var shield_system_path: NodePath
 
@@ -12,6 +14,8 @@ extends Control
 @onready var _wind: WindSystem = get_node(wind_system_path)
 @onready var _platform: PlatformController = get_node(platform_path)
 @onready var _anchors: AnchorSystem = get_node(anchor_system_path)
+@onready var _crew_roles: CrewRoleManager = get_node(crew_role_manager_path)
+@onready var _crew_input: CrewDebugInput = get_node(crew_debug_input_path)
 @onready var _world: PrototypeWorld = get_node(world_path)
 @onready var _shield: PrototypeShieldSystem = get_node(shield_system_path)
 
@@ -19,6 +23,7 @@ extends Control
 @onready var _wind_label: Label = %WindLabel
 @onready var _platform_label: Label = %PlatformLabel
 @onready var _anchor_label: Label = %AnchorLabel
+@onready var _crew_label: Label = %CrewLabel
 @onready var _contact_label: Label = %ContactLabel
 @onready var _shield_label: Label = %ShieldLabel
 @onready var _pause_label: Label = %PauseLabel
@@ -49,6 +54,11 @@ func _process(_delta: float) -> void:
 	_anchor_label.text = "Якоря: %s | зона: %s" % [
 		_anchors.get_state_summary(),
 		"ДА" if _anchors.is_in_installation_zone() else "НЕТ",
+	]
+
+	_crew_label.text = "Экипаж: %s | выбран: %d" % [
+		_crew_roles.get_summary(),
+		_crew_input.selected_defender_id + 1,
 	]
 
 	_contact_label.text = "Энергетический контакт: %s" % (
