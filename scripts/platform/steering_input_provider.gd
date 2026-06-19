5c123464822cc9e0f1ce5eb327230cf02895c518
bageus <36338626+bageus@users.noreply.github.com>
2026-06-19 22:06:07 +0200

    refactor: separate steering input from platform movement

---
+++ scripts/platform/steering_input_provider.gd
@@ -0,0 +1,27 @@
+class_name SteeringInputProvider
+extends Node
+
+signal driver_availability_changed(is_available: bool)
+
+@export var left_action: StringName = &"ui_left"
+@export var right_action: StringName = &"ui_right"
+@export var driver_available: bool = true
+
+
+func get_steering_axis() -> float:
+	if not driver_available:
+		return 0.0
+	return Input.get_axis(left_action, right_action)
+
+
+func is_control_input_active() -> bool:
+	if not driver_available:
+		return false
+	return Input.is_action_pressed(left_action) or Input.is_action_pressed(right_action)
+
+
+func set_driver_available(value: bool) -> void:
+	if driver_available == value:
+		return
+	driver_available = value
+	driver_availability_changed.emit(driver_available)
