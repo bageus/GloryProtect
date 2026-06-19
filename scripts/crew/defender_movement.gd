913d3922885056d7f3bc405111f1c91bcee657d9
bageus <36338626+bageus@users.noreply.github.com>
2026-06-20 00:48:26 +0200

    feat: add enemy blocking and resumable defender movement

--- scripts/crew/defender_movement.gd
+++ scripts/crew/defender_movement.gd
@@ -8,6 +8,9 @@ signal destination_reached
 
 var _target_x: float = 0.0
 var _moving: bool = false
+var _paused: bool = false
+var _defender: Defender = null
+var _movement_resolver: BoardingMovementResolver = null
 
 @onready var _actor: Node2D = get_parent() as Node2D
 
@@ -16,8 +19,17 @@ func configure(new_move_speed: float) -> void:
 	move_speed = maxf(0.0, new_move_speed)
 
 
+func configure_collision(
+	defender: Defender,
+	movement_resolver: BoardingMovementResolver
+) -> void:
+	_defender = defender
+	_movement_resolver = movement_resolver
+
+
 func move_to(target_x: float) -> void:
 	_target_x = target_x
+	_paused = false
 	if absf(_actor.position.x - _target_x) <= arrival_epsilon:
 		_actor.position.x = _target_x
 		_moving = false
@@ -26,8 +38,19 @@ func move_to(target_x: float) -> void:
 	_moving = true
 
 
+func pause() -> void:
+	if _moving:
+		_paused = true
+
+
+func resume() -> void:
+	if _moving:
+		_paused = false
+
+
 func stop() -> void:
 	_moving = false
+	_paused = false
 	_target_x = _actor.position.x
 
 
@@ -35,10 +58,15 @@ func teleport_to(target_x: float) -> void:
 	_actor.position.x = target_x
 	_target_x = target_x
 	_moving = false
+	_paused = false
 
 
 func is_moving() -> bool:
 	return _moving
+
+
+func is_paused() -> bool:
+	return _paused
 
 
 func get_target_x() -> float:
@@ -46,18 +74,25 @@ func get_target_x() -> float:
 
 
 func _physics_process(delta: float) -> void:
-	if not _moving:
+	if not _moving or _paused:
 		return
 
-	_actor.position.x = move_toward(
+	var desired_x: float = move_toward(
 		_actor.position.x,
 		_target_x,
 		move_speed * delta
 	)
+	if _movement_resolver != null and _defender != null:
+		desired_x = _movement_resolver.resolve_defender_platform_x(
+			_defender,
+			_actor.position.x,
+			desired_x
+		)
+	_actor.position.x = desired_x
 
 	if absf(_actor.position.x - _target_x) > arrival_epsilon:
 		return
-
 	_actor.position.x = _target_x
 	_moving = false
+	_paused = false
 	destination_reached.emit()
