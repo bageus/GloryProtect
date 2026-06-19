366c234a6ee67468636aea7b38c60f67a2d2eb73
bageus <36338626+bageus@users.noreply.github.com>
2026-06-19 22:15:34 +0200

    refactor: keep station geometry in platform balance

--- scripts/crew/crew_balance.gd
+++ scripts/crew/crew_balance.gd
@@ -6,4 +6,3 @@ extends Resource
 @export_range(20.0, 600.0, 1.0) var defender_move_speed: float = 180.0
 @export_range(-200.0, 100.0, 1.0) var defender_local_y: float = -48.0
 @export_range(4.0, 40.0, 1.0) var defender_body_radius: float = 14.0
-@export_range(0.0, 5.0, 0.1) var anchor_station_cell_inset: float = 1.5
