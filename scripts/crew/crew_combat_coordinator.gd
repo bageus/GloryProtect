74a5f5e00acd0d26507351ad74c2233b394050c8
bageus <36338626+bageus@users.noreply.github.com>
2026-06-19 23:36:02 +0200

    feat: configure defender combat components

---
+++ scripts/crew/crew_combat_coordinator.gd
@@ -0,0 +1,44 @@
+class_name CrewCombatCoordinator
+extends Node
+
+@export_node_path("GameFlowController") var game_flow_path: NodePath
+@export_node_path("CrewManager") var crew_manager_path: NodePath
+@export_node_path("CrewRoleManager") var role_manager_path: NodePath
+@export_node_path("BoardingEnemyRegistry") var enemy_registry_path: NodePath
+@export var balance: BoardingBalance
+
+@onready var _game_flow: GameFlowController = get_node(game_flow_path)
+@onready var _crew: CrewManager = get_node(crew_manager_path)
+@onready var _roles: CrewRoleManager = get_node(role_manager_path)
+@onready var _enemies: BoardingEnemyRegistry = get_node(enemy_registry_path)
+
+
+func _ready() -> void:
+	assert(balance != null, "CrewCombatCoordinator requires BoardingBalance")
+	_crew.defender_spawned.connect(_on_defender_spawned)
+	call_deferred("_configure_existing_defenders")
+
+
+func _configure_existing_defenders() -> void:
+	for defender: Defender in _crew.get_all_defenders():
+		_configure_defender(defender)
+
+
+func _configure_defender(defender: Defender) -> void:
+	defender.melee.configure(
+		balance.defender_attack_damage,
+		balance.defender_attack_windup,
+		balance.defender_attack_cooldown
+	)
+	defender.combat.configure(
+		defender,
+		balance,
+		_game_flow,
+		_roles,
+		_enemies,
+		defender.melee
+	)
+
+
+func _on_defender_spawned(_defender_id: int, defender: Defender) -> void:
+	_configure_defender(defender)
