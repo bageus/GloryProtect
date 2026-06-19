07c6a6970547262908c34673e2472df54cd08bdd
bageus <36338626+bageus@users.noreply.github.com>
2026-06-20 00:12:39 +0200

    feat: add defender replacement balance parameters

--- scripts/crew/crew_balance.gd
+++ scripts/crew/crew_balance.gd
@@ -2,7 +2,10 @@ class_name CrewBalance
 extends Resource
 
 @export_range(1, 12, 1) var starting_defender_count: int = 3
+@export_range(1, 12, 1) var maximum_defender_count: int = 3
 @export_range(1, 10, 1) var defender_max_health: int = 3
 @export_range(20.0, 600.0, 1.0) var defender_move_speed: float = 180.0
 @export_range(-200.0, 100.0, 1.0) var defender_local_y: float = -48.0
 @export_range(4.0, 40.0, 1.0) var defender_body_radius: float = 14.0
+@export_range(0.1, 120.0, 0.1) var replacement_delay_seconds: float = 12.0
+@export_range(-500.0, 500.0, 1.0) var replacement_door_local_x: float = 0.0
