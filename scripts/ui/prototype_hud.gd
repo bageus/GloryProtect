ea746d962d2c5a7a272fdae49e3b3041b9cb35b2
bageus <36338626+bageus@users.noreply.github.com>
2026-06-19 21:13:54 +0200

    feat: show anchor telemetry in prototype HUD

--- scripts/ui/prototype_hud.gd
+++ scripts/ui/prototype_hud.gd
@@ -4,18 +4,21 @@ extends Control
 @export_node_path("GameFlowController") var game_flow_path: NodePath
 @export_node_path("WindSystem") var wind_system_path: NodePath
 @export_node_path("PlatformController") var platform_path: NodePath
+@export_node_path("AnchorSystem") var anchor_system_path: NodePath
 @export_node_path("PrototypeWorld") var world_path: NodePath
 @export_node_path("PrototypeShieldSystem") var shield_system_path: NodePath
 
 @onready var _game_flow: GameFlowController = get_node(game_flow_path)
 @onready var _wind: WindSystem = get_node(wind_system_path)
 @onready var _platform: PlatformController = get_node(platform_path)
+@onready var _anchors: AnchorSystem = get_node(anchor_system_path)
 @onready var _world: PrototypeWorld = get_node(world_path)
 @onready var _shield: PrototypeShieldSystem = get_node(shield_system_path)
 
 @onready var _state_label: Label = %StateLabel
 @onready var _wind_label: Label = %WindLabel
 @onready var _platform_label: Label = %PlatformLabel
+@onready var _anchor_label: Label = %AnchorLabel
 @onready var _contact_label: Label = %ContactLabel
 @onready var _shield_label: Label = %ShieldLabel
 @onready var _pause_label: Label = %PauseLabel
@@ -41,6 +44,11 @@ func _process(_delta: float) -> void:
 		_platform.position.x,
 		_platform.horizontal_velocity,
 		_platform.steering_axis,
+	]
+
+	_anchor_label.text = "Якоря: %s | зона: %s" % [
+		_anchors.get_state_summary(),
+		"ДА" if _anchors.is_in_installation_zone() else "НЕТ",
 	]
 
 	_contact_label.text = "Энергетический контакт: %s" % (
