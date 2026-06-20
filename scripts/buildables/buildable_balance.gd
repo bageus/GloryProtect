c426949a8ec8fae975e9fb413522c0c853b9b23a
bageus <36338626+bageus@users.noreply.github.com>
2026-06-20 19:09:53 +0200

    feat: add turret balance parameters

--- scripts/buildables/buildable_balance.gd
+++ scripts/buildables/buildable_balance.gd
@@ -3,6 +3,7 @@ extends Resource
 
 @export_group("Inventory")
 @export_range(0, 8, 1) var medical_station_max_count: int = 1
+@export_range(0, 16, 1) var turret_max_count: int = 4
 @export_range(0, 32, 1) var default_medical_cell: int = 11
 @export var reserved_cell_indices: Array[int] = [1, 6, 7, 8, 9, 16]
 
@@ -14,13 +15,22 @@ extends Resource
 @export_range(20.0, 160.0, 1.0) var medical_station_height: float = 66.0
 @export_range(-120.0, 20.0, 1.0) var medical_station_bottom_y: float = -28.0
 
+@export_group("Turret")
+@export_range(1, 10, 1) var turret_damage: int = 1
+@export_range(40.0, 1000.0, 1.0) var turret_range: float = 360.0
+@export_range(0.05, 5.0, 0.05) var turret_shot_windup: float = 0.45
+@export_range(0.0, 10.0, 0.05) var turret_shot_cooldown: float = 0.8
+@export_range(10.0, 120.0, 1.0) var turret_width: float = 42.0
+@export_range(10.0, 140.0, 1.0) var turret_height: float = 48.0
+@export_range(-120.0, 20.0, 1.0) var turret_bottom_y: float = -28.0
+
 
 func get_max_count(type_id: int) -> int:
 	match type_id:
 		BuildableType.Id.MEDICAL_STATION:
 			return medical_station_max_count
 		BuildableType.Id.TURRET:
-			return 0
+			return turret_max_count
 		_:
 			return 0
 
