c80fa75a82aa69527901ebfb9e1c8778d9745c87
bageus <36338626+bageus@users.noreply.github.com>
2026-06-20 13:32:01 +0200

    feat: support dynamic crew role stations

--- scripts/crew/role_station_registry.gd
+++ scripts/crew/role_station_registry.gd
@@ -2,16 +2,36 @@ class_name RoleStationRegistry
 extends RefCounted
 
 var _platform: PlatformController
-var _owners: Dictionary = {}
+var _owners: Dictionary[int, int] = {}
+var _dynamic_targets: Dictionary[int, float] = {}
 
 
 func configure(platform: PlatformController) -> void:
 	_platform = platform
 
 
+func set_dynamic_target(role_id: int, local_x: float) -> void:
+	_dynamic_targets[role_id] = local_x
+
+
+func clear_dynamic_target(role_id: int) -> void:
+	_dynamic_targets.erase(role_id)
+	_owners.erase(role_id)
+
+
+func has_station(role_id: int) -> bool:
+	if not CrewRole.is_fixed_station(role_id):
+		return true
+	if role_id == CrewRole.Id.MEDIC:
+		return _dynamic_targets.has(role_id)
+	return true
+
+
 func reserve(role_id: int, defender_id: int) -> bool:
 	if not CrewRole.is_fixed_station(role_id):
 		return true
+	if not has_station(role_id):
+		return false
 	if _owners.has(role_id) and _owners[role_id] != defender_id:
 		return false
 	_owners[role_id] = defender_id
@@ -30,6 +50,8 @@ func get_owner(role_id: int) -> int:
 
 
 func get_target_x(role_id: int, defender_id: int) -> float:
+	if _dynamic_targets.has(role_id):
+		return _dynamic_targets[role_id]
 	var platform_width := _platform.get_platform_width()
 	var post_offset := (
 		platform_width * 0.5
