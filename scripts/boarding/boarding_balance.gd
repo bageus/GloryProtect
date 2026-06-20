472a46cabd1dfd8675ff774f47937078859a6b43
bageus <36338626+bageus@users.noreply.github.com>
2026-06-20 23:39:14 +0200

    feat: reference enemy archetype catalog from boarding balance

--- scripts/boarding/boarding_balance.gd
+++ scripts/boarding/boarding_balance.gd
@@ -1,11 +1,16 @@
 class_name BoardingBalance
 extends Resource
 
+@export var enemy_catalog: BoardingEnemyCatalog
+
+@export_group("Legacy Base Enemy Defaults")
 @export_range(1, 20, 1) var enemy_max_health: int = 1
 @export_range(20.0, 500.0, 1.0) var ground_move_speed: float = 125.0
 @export_range(20.0, 500.0, 1.0) var climb_move_speed: float = 105.0
 @export_range(20.0, 500.0, 1.0) var platform_move_speed: float = 90.0
 @export_range(4.0, 40.0, 1.0) var enemy_body_radius: float = 12.0
+
+@export_group("Spawn")
 @export_range(0.0, 100.0, 1.0) var ground_vertical_offset: float = 12.0
 @export_range(-100.0, 100.0, 1.0) var platform_local_y: float = -48.0
 @export_range(0.0, 100.0, 1.0) var ground_arrival_epsilon: float = 8.0
@@ -28,7 +33,7 @@ extends Resource
 @export_range(0.0, 40.0, 1.0) var jump_trigger_tolerance: float = 4.0
 @export_range(20.0, 240.0, 1.0) var jump_max_horizontal_distance: float = 120.0
 
-@export_group("Enemy Combat")
+@export_group("Legacy Base Enemy Combat")
 @export_range(1, 20, 1) var enemy_attack_damage: int = 1
 @export_range(0.05, 5.0, 0.05) var enemy_attack_windup: float = 0.55
 @export_range(0.05, 5.0, 0.05) var enemy_attack_cooldown: float = 0.85
