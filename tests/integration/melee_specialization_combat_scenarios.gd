466f7e3f57468fd7c56272b971d49b7969622dce
bageus <36338626+bageus@users.noreply.github.com>
2026-06-21 19:54:57 +0200

    Add melee specialization combat integration scenarios

---
+++ tests/integration/melee_specialization_combat_scenarios.gd
@@ -0,0 +1,106 @@
+extends SceneTree
+
+const GAME_SCENE := preload("res://scenes/game/game_root_with_flyers.tscn")
+
+
+func _init() -> void:
+	call_deferred("_run_scenarios")
+
+
+func _run_scenarios() -> void:
+	await _test_duelist_double_attack_is_sequential()
+	await _test_assault_hits_forward_group_and_rear_enemy()
+	print("Melee specialization combat scenarios passed")
+	quit()
+
+
+func _test_duelist_double_attack_is_sequential() -> void:
+	var game: Node2D = await _create_stable_game()
+	var crew: CrewManager = game.get_node("World/Platform/CrewManager")
+	var director: BoardingSpawnDirector = game.get_node("World/BoardingSpawnDirector")
+	assert(crew.apply_melee_flag(&"melee_specialization_duelist"))
+	assert(crew.apply_melee_flag(&"melee_duelist_double_attack"))
+	var defender: Defender = crew.get_defender(0)
+	defender.teleport_to(0.0)
+	var target: BoardingEnemy = _spawn_boarded_enemy(director, 12.0)
+	target.health.configure(5)
+
+	assert(bool(defender.combat.call("_try_start_attack", target)))
+	defender.melee.tick(0.4)
+	assert(target.health.current_health == 4)
+	assert(defender.melee.is_attacking())
+	assert(defender.combat.get_completed_hit_count() == 1)
+
+	defender.melee.tick(0.4)
+	assert(target.health.current_health == 3)
+	assert(not defender.melee.is_attacking())
+	assert(defender.combat.get_completed_hit_count() == 2)
+
+	game.queue_free()
+	await process_frame
+
+
+func _test_assault_hits_forward_group_and_rear_enemy() -> void:
+	var game: Node2D = await _create_stable_game()
+	var crew: CrewManager = game.get_node("World/Platform/CrewManager")
+	var director: BoardingSpawnDirector = game.get_node("World/BoardingSpawnDirector")
+	assert(crew.apply_melee_flag(&"melee_specialization_assault"))
+	assert(crew.apply_melee_flag(&"melee_assault_back_attack"))
+	var defender: Defender = crew.get_defender(0)
+	defender.teleport_to(0.0)
+
+	var primary: BoardingEnemy = _spawn_boarded_enemy(director, 12.0)
+	var behind_one: BoardingEnemy = _spawn_boarded_enemy(director, 24.0)
+	var behind_two: BoardingEnemy = _spawn_boarded_enemy(director, 36.0)
+	var behind_three: BoardingEnemy = _spawn_boarded_enemy(director, 48.0)
+	var behind_four: BoardingEnemy = _spawn_boarded_enemy(director, 60.0)
+	var rear: BoardingEnemy = _spawn_boarded_enemy(director, -12.0)
+
+	assert(bool(defender.combat.call("_try_start_attack", primary)))
+	defender.melee.tick(0.4)
+	assert(primary.health.current_health == 1)
+	assert(behind_one.health.current_health == 2)
+	assert(behind_two.health.current_health == 2)
+	assert(behind_three.health.current_health == 2)
+	assert(behind_four.health.current_health == 3)
+	assert(rear.health.current_health == 2)
+
+	game.queue_free()
+	await process_frame
+
+
+func _create_stable_game() -> Node2D:
+	var game: Node2D = GAME_SCENE.instantiate() as Node2D
+	var flow: GameFlowController = game.get_node("GameFlowController")
+	flow.start_delay_seconds = 0.0
+	root.add_child(game)
+	await process_frame
+	await process_frame
+	flow.state = GameFlowController.RunState.RUNNING
+	var director: BoardingSpawnDirector = game.get_node("World/BoardingSpawnDirector")
+	director.set_process(false)
+	director.set_physics_process(false)
+	var node_paths: Array[NodePath] = [
+		NodePath("World/StrategicWaveSystem"),
+		NodePath("World/StrategicWaveDirector"),
+		NodePath("World/StrategicGroupMutationController"),
+	]
+	for path: NodePath in node_paths:
+		var system: Node = game.get_node(path)
+		system.set_process(false)
+		system.set_physics_process(false)
+	var crew: CrewManager = game.get_node("World/Platform/CrewManager")
+	for defender: Defender in crew.get_all_defenders():
+		defender.combat.set_physics_process(false)
+	return game
+
+
+func _spawn_boarded_enemy(
+	director: BoardingSpawnDirector,
+	local_x: float
+) -> BoardingEnemy:
+	var enemy: BoardingEnemy = director.spawn_debug_archetype(&"brute", 1)
+	assert(enemy != null)
+	enemy.force_board_at(local_x)
+	enemy.controller.set_physics_process(false)
+	return enemy
