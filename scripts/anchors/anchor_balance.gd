e947e8f9e6064fdccb002e49b8224598a0b96640
bageus <36338626+bageus@users.noreply.github.com>
2026-06-21 02:53:31 +0200

    feat: add data-driven rope durability balance

--- scripts/anchors/anchor_balance.gd
+++ scripts/anchors/anchor_balance.gd
@@ -6,6 +6,7 @@ extends Resource
 @export_range(0.1, 10.0, 0.1) var overload_duration: float = 2.5
 @export_range(0.1, 5.0, 0.1) var return_duration: float = 0.75
 @export_range(100.0, 1000.0, 1.0) var rope_length: float = 315.0
+@export_range(1.0, 1000.0, 1.0) var rope_max_durability: float = 100.0
 @export_range(0.0, 200.0, 1.0) var overload_stretch: float = 42.0
 @export_range(0.1, 20.0, 0.1) var tension_epsilon: float = 2.0
 @export var ground_offsets := PackedFloat32Array([-230.0, -125.0, 125.0, 230.0])
