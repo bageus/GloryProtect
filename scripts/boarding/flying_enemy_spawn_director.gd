05dc2aa8d0ee86c9f40fc6053ed3d50d2688171d
bageus <36338626+bageus@users.noreply.github.com>
2026-06-21 10:54:18 +0200

    Add flying enemy spawn director

---
+++ scripts/boarding/flying_enemy_spawn_director.gd
@@ -0,0 +1,81 @@
+class_name FlyingEnemySpawnDirector
+extends Node
+
+@export_node_path("GameFlowController") var game_flow_path: NodePath
+@export_node_path("PlatformController") var platform_path: NodePath
+@export_node_path("BoardingEnemyRegistry") var enemy_registry_path: NodePath
+@export_node_path("BoardingMovementResolver") var movement_resolver_path: NodePath
+@export_node_path("BoardingJumpPlanner") var jump_planner_path: NodePath
+@export_node_path("CrewManager") var crew_manager_path: NodePath
+@export_node_path("GroundOrbRegistry") var orb_registry_path: NodePath
+@export var enemy_container_path: NodePath
+@export var enemy_scene: PackedScene
+@export var boarding_balance: BoardingBalance
+@export var profile: FlyingEnemyProfile
+
+var _spawn_remaining: float = 0.0
+var _rng := RandomNumberGenerator.new()
+
+@onready var _game_flow: GameFlowController = get_node(game_flow_path)
+@onready var _platform: PlatformController = get_node(platform_path)
+@onready var _registry: BoardingEnemyRegistry = get_node(enemy_registry_path)
+@onready var _movement_resolver: BoardingMovementResolver = get_node(movement_resolver_path)
+@onready var _jump_planner: BoardingJumpPlanner = get_node(jump_planner_path)
+@onready var _crew: CrewManager = get_node(crew_manager_path)
+@onready var _orbs: GroundOrbRegistry = get_node(orb_registry_path)
+@onready var _container: Node2D = get_node(enemy_container_path)
+
+
+func _ready() -> void:
+	assert(enemy_scene != null, "FlyingEnemySpawnDirector requires enemy scene")
+	assert(boarding_balance != null, "FlyingEnemySpawnDirector requires boarding balance")
+	assert(profile != null and profile.is_valid(), "Flying enemy profile is invalid")
+	_rng.randomize()
+	_spawn_remaining = profile.spawn_interval
+
+
+func _physics_process(delta: float) -> void:
+	if not _game_flow.is_world_simulation_active():
+		return
+	_spawn_remaining = maxf(0.0, _spawn_remaining - delta)
+	if _spawn_remaining > 0.0:
+		return
+	spawn_now()
+	_spawn_remaining = profile.spawn_interval
+
+
+func spawn_now(side: int = 0) -> BoardingEnemy:
+	var resolved_side: int = side
+	if resolved_side == 0:
+		resolved_side = -1 if _rng.randf() < 0.5 else 1
+	var enemy: BoardingEnemy = enemy_scene.instantiate() as BoardingEnemy
+	assert(enemy != null, "Flying enemy scene root must use BoardingEnemy")
+	_container.add_child(enemy)
+	enemy.configure(
+		profile.archetype,
+		boarding_balance,
+		_game_flow,
+		_platform,
+		get_node("../AnchorPathRegistry") as AnchorPathRegistry,
+		_crew,
+		_orbs,
+		_movement_resolver,
+		_jump_planner
+	)
+	var behavior := FlyingEnemyBehavior.new()
+	behavior.setup(profile, _platform, _crew, _registry, enemy.melee)
+	enemy.attach_special_behavior(behavior, _game_flow)
+	_registry.register_enemy(enemy)
+	enemy.global_position = Vector2(
+		_platform.global_position.x + float(resolved_side) * profile.spawn_distance,
+		_platform.global_position.y - profile.hover_height
+	)
+	return enemy
+
+
+func reset_spawn_timer() -> void:
+	_spawn_remaining = profile.spawn_interval
+
+
+func get_spawn_remaining() -> float:
+	return maxf(0.0, _spawn_remaining)
