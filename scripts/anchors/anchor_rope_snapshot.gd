34fb2fd3a754950b3febc77fff13bf9cbbdec73c
bageus <36338626+bageus@users.noreply.github.com>
2026-06-21 03:06:47 +0200

    refactor: make rope snapshots read-only

--- scripts/anchors/anchor_rope_snapshot.gd
+++ scripts/anchors/anchor_rope_snapshot.gd
@@ -1,11 +1,27 @@
 class_name AnchorRopeSnapshot
 extends RefCounted
 
-var anchor_id: int
-var current_durability: float
-var maximum_durability: float
-var durability_ratio: float
-var is_destroyed: bool
+var _anchor_id: int
+var _current_durability: float
+var _maximum_durability: float
+var _durability_ratio: float
+var _is_destroyed: bool
+
+var anchor_id: int:
+	get:
+		return _anchor_id
+var current_durability: float:
+	get:
+		return _current_durability
+var maximum_durability: float:
+	get:
+		return _maximum_durability
+var durability_ratio: float:
+	get:
+		return _durability_ratio
+var is_destroyed: bool:
+	get:
+		return _is_destroyed
 
 
 func _init(
@@ -13,14 +29,14 @@ func _init(
 	new_current_durability: float,
 	new_maximum_durability: float
 ) -> void:
-	anchor_id = new_anchor_id
-	current_durability = new_current_durability
-	maximum_durability = new_maximum_durability
-	durability_ratio = 0.0
-	if maximum_durability > 0.0:
-		durability_ratio = clampf(
-			current_durability / maximum_durability,
+	_anchor_id = new_anchor_id
+	_current_durability = new_current_durability
+	_maximum_durability = new_maximum_durability
+	_durability_ratio = 0.0
+	if _maximum_durability > 0.0:
+		_durability_ratio = clampf(
+			_current_durability / _maximum_durability,
 			0.0,
 			1.0
 		)
-	is_destroyed = current_durability <= 0.0
+	_is_destroyed = _current_durability <= 0.0
