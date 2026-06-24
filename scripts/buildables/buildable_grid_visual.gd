cccf3d77d58bfd7a8ad2ab710b1593cf1f023735
bageus <36338626+bageus@users.noreply.github.com>
2026-06-25 00:02:20 +0200

    Render mouse placement cell states

--- scripts/buildables/buildable_grid_visual.gd
+++ scripts/buildables/buildable_grid_visual.gd
@@ -11,6 +11,7 @@ const ALPHA_CROP_THRESHOLD: float = 0.08
 @export_node_path("BuildableInventory") var inventory_path: NodePath
 @export_node_path("BuildableDebugInput") var debug_input_path: NodePath
 @export_node_path("TurretDebugInput") var turret_input_path: NodePath
+@export_node_path("BuildablePlacementController") var placement_controller_path: NodePath
 @export_node_path("TurretSystem") var turret_system_path: NodePath = NodePath("../../TurretSystem")
 @export_node_path("BoardingEnemyRegistry") var enemy_registry_path: NodePath = NodePath("../../BoardingEnemyRegistry")
 @export var balance: BuildableBalance
@@ -20,8 +21,13 @@ const ALPHA_CROP_THRESHOLD: float = 0.08
 @onready var _platform: PlatformController = get_node(platform_path)
 @onready var _grid: BuildableGrid = get_node(grid_path)
 @onready var _inventory: BuildableInventory = get_node(inventory_path)
-@onready var _debug_input: BuildableDebugInput = get_node(debug_input_path)
+@onready var _debug_input: BuildableDebugInput = get_node_or_null(
+	debug_input_path
+) as BuildableDebugInput
 @onready var _turret_input: TurretDebugInput = get_node(turret_input_path)
+@onready var _placement: BuildablePlacementController = get_node_or_null(
+	placement_controller_path
+) as BuildablePlacementController
 @onready var _turrets: TurretSystem = get_node(turret_system_path)
 @onready var _enemies: BoardingEnemyRegistry = get_node(enemy_registry_path)
 
@@ -37,16 +43,28 @@ func _ready() -> void:
 	_grid.grid_reset.connect(_on_grid_reset)
 	_inventory.buildable_unlocked.connect(_on_inventory_changed)
 	_inventory.inventory_reset.connect(_on_grid_reset)
-	_debug_input.selected_cell_changed.connect(_on_selected_cell_changed)
+	if _placement != null:
+		_placement.mode_changed.connect(_on_placement_changed)
+		_placement.hovered_cell_changed.connect(_on_selected_cell_changed)
+		_placement.selected_buildable_changed.connect(_on_selected_buildable_changed)
+	elif _debug_input != null:
+		_debug_input.selected_cell_changed.connect(_on_selected_cell_changed)
 	_create_turret_visual()
 	queue_redraw()
 
 
 func _draw() -> void:
-	_draw_selected_cell()
+	if _placement != null and _placement.is_grid_preview_visible():
+		_draw_placement_grid()
+	elif _debug_input != null:
+		_draw_selected_cell()
 	for snapshot: BuildableSnapshot in _grid.get_snapshots():
 		if snapshot.type_id == BuildableType.Id.MEDICAL_STATION:
-			_draw_medical_station(snapshot.local_x)
+			_draw_medical_station(
+				snapshot,
+				_placement != null
+				and snapshot.buildable_id == _placement.get_selected_buildable_id()
+			)
 
 
 func _create_turret_visual() -> void:
@@ -63,37 +81,51 @@ func _create_turret_visual() -> void:
 	add_child(turret_visual)
 
 
+func _draw_placement_grid() -> void:
+	var hovered_cell := _placement.get_hovered_cell_index()
+	var selected_id := _placement.get_selected_buildable_id()
+	for cell_index: int in range(_platform.get_cell_count()):
+		var occupant_id := _grid.get_buildable_id_at_cell(cell_index)
+		var reason := _placement.get_cell_unavailability_reason(cell_index)
+		var color := Color(0.32, 0.35, 0.4, 0.72)
+		if reason == &"":
+			color = Color(0.24, 0.9, 0.62, 0.86)
+		elif reason == BuildableGrid.REASON_CELL_OCCUPIED:
+			color = Color(0.25, 0.72, 1.0, 0.88)
+		elif reason == BuildableGrid.REASON_CELL_NOT_ALLOWED:
+			color = Color(0.65, 0.28, 0.3, 0.62)
+		else:
+			color = Color(0.92, 0.42, 0.24, 0.76)
+		if occupant_id >= 0:
+			color = Color(0.25, 0.72, 1.0, 0.88)
+		if occupant_id == selected_id:
+			color = Color(1.0, 0.82, 0.25, 0.95)
+		var fill_alpha := 0.1
+		var line_width := 1.5
+		if cell_index == hovered_cell:
+			fill_alpha = 0.24
+			line_width = 3.0
+		var rect := _platform.get_cell_rect_local(cell_index)
+		draw_rect(rect, Color(color.r, color.g, color.b, fill_alpha), true)
+		draw_rect(rect, color, false, line_width)
+
+
 func _draw_selected_cell() -> void:
 	var cell_index: int = _debug_input.get_selected_cell_index()
-	var local_x: float = _platform.get_cell_local_x(cell_index)
-	var top_y: float = -_platform.get_platform_height() * 0.5
 	var color := Color(0.95, 0.34, 0.22, 0.8)
 	if _grid.is_cell_available_for_type(BuildableType.Id.TURRET, cell_index):
 		color = Color(0.25, 0.9, 0.72, 0.8)
-	if _get_buildable_at_cell(cell_index) >= 0:
+	if _grid.get_buildable_id_at_cell(cell_index) >= 0:
 		color = Color(0.28, 0.8, 1.0, 0.9)
-	var rect := Rect2(
-		Vector2(
-			local_x - _platform.balance.cell_width * 0.5,
-			top_y
-		),
-		Vector2(_platform.balance.cell_width, _platform.get_platform_height())
-	)
+	var rect := _platform.get_cell_rect_local(cell_index)
 	draw_rect(rect, Color(color.r, color.g, color.b, 0.12), true)
 	draw_rect(rect, color, false, 2.0)
 
 
-func _get_buildable_at_cell(cell_index: int) -> int:
-	for snapshot: BuildableSnapshot in _grid.get_snapshots():
-		if snapshot.cell_index == cell_index:
-			return snapshot.buildable_id
-	return -1
-
-
-func _draw_medical_station(local_x: float) -> void:
+func _draw_medical_station(snapshot: BuildableSnapshot, selected: bool) -> void:
 	var asset_size: Vector2 = _medical_source_rect.size * medical_post_scale
 	var bottom_center := Vector2(
-		local_x + medical_post_surface_offset.x,
+		snapshot.local_x + medical_post_surface_offset.x,
 		balance.medical_station_bottom_y + medical_post_surface_offset.y
 	)
 	var rect := Rect2(
@@ -105,6 +137,8 @@ func _draw_medical_station(local_x: float) -> void:
 		rect,
 		_medical_source_rect
 	)
+	if selected:
+		draw_rect(rect.grow(4.0), Color(1.0, 0.84, 0.3), false, 3.0)
 
 
 func _get_alpha_bounds(texture: Texture2D) -> Rect2:
@@ -143,6 +177,14 @@ func _on_inventory_changed(_type_id: int, _count: int) -> void:
 
 
 func _on_selected_cell_changed(_cell_index: int) -> void:
+	queue_redraw()
+
+
+func _on_selected_buildable_changed(_buildable_id: int) -> void:
+	queue_redraw()
+
+
+func _on_placement_changed(_mode: int, _type_id: int, _buildable_id: int) -> void:
 	queue_redraw()
 
 
