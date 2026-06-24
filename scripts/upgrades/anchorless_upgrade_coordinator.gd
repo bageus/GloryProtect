eda98549e7e63a51c1aa218f6d0d1f933d01f536
bageus <36338626+bageus@users.noreply.github.com>
2026-06-25 00:16:10 +0200

    Configure anchorless upgrade coordinator

--- scripts/upgrades/anchorless_upgrade_coordinator.gd
+++ scripts/upgrades/anchorless_upgrade_coordinator.gd
@@ -1,2 +1,13 @@
 class_name AnchorlessUpgradeCoordinator
 extends ShieldCoreUpgradeCoordinator
+
+@export_node_path("AnchorlessControlSystem") var anchorless_system_path: NodePath
+
+@onready var anchorless_system: AnchorlessControlSystem = get_node(anchorless_system_path)
+
+
+func _ready() -> void:
+	_effect_applier = AnchorlessUpgradeEffectApplier.new()
+	super._ready()
+	var adapter := _effect_applier as AnchorlessUpgradeEffectApplier
+	adapter.set_anchorless_system(anchorless_system)
