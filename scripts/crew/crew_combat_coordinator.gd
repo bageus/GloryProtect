ecd34feeffd4784e6bf63458f5663082366749e9
bageus <36338626+bageus@users.noreply.github.com>
2026-06-24 09:16:15 +0200

    Route base melee configuration through Defender

--- scripts/crew/crew_combat_coordinator.gd
+++ scripts/crew/crew_combat_coordinator.gd
@@ -30,7 +30,7 @@ func _configure_existing_defenders() -> void:
 
 func _configure_defender(defender: Defender) -> void:
 	defender.movement.configure_collision(defender, _movement_resolver)
-	defender.melee.configure(
+	defender.set_base_melee_configuration(
 		balance.defender_attack_damage,
 		balance.defender_attack_windup,
 		balance.defender_attack_cooldown
@@ -42,6 +42,7 @@ func _configure_defender(defender: Defender) -> void:
 		_platform,
 		_roles,
 		_enemies,
+		_movement_resolver,
 		defender.melee
 	)
 	defender.shooter_combat.configure(
