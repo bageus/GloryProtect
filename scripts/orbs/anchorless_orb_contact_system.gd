b21aad60329a3798b800bddce713229ca4d23d19
bageus <36338626+bageus@users.noreply.github.com>
2026-06-25 00:18:38 +0200

    Preserve contact while simulation is paused

--- scripts/orbs/anchorless_orb_contact_system.gd
+++ scripts/orbs/anchorless_orb_contact_system.gd
@@ -1,2 +1,8 @@
 class_name AnchorlessOrbContactSystem
 extends OrbContactSystem
+
+
+func _physics_process(_delta: float) -> void:
+	if not _game_flow.is_world_simulation_active():
+		return
+	_set_active_orb(_registry.get_contact_orb_at(_platform.position.x))
