3f229b4ecc5c04f9937a04993c50297a26075940
bageus <36338626+bageus@users.noreply.github.com>
2026-06-25 22:29:36 +0200

    Use fixed medical anchor in emergency cycle test

--- tests/integration/medic_emergency_cycle_scenarios.gd
+++ tests/integration/medic_emergency_cycle_scenarios.gd
@@ -25,10 +25,8 @@ func _run_scenario() -> void:
 	medical.set_physics_process(false)
 
 	assert(inventory.unlock(BuildableType.Id.MEDICAL_STATION, 1) == 1)
-	assert(grid.place(
-		BuildableType.Id.MEDICAL_STATION,
-		grid.balance.default_medical_cell
-	) >= 0)
+	var medical_anchor: int = grid.balance.get_medical_cell_indices()[0]
+	assert(grid.place(BuildableType.Id.MEDICAL_STATION, medical_anchor) >= 0)
 	await process_frame
 	assert(medical.apply_upgrade_effect(_flag_effect(
 		MedicUpgradeRuntime.FIELD
