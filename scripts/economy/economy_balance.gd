16ca69c37134902fb7ac861753ba37ff86a6d526
bageus <36338626+bageus@users.noreply.github.com>
2026-06-25 22:31:19 +0200

    Add strategic shield-impact reward balance

--- scripts/economy/economy_balance.gd
+++ scripts/economy/economy_balance.gd
@@ -3,6 +3,7 @@ extends Resource
 
 @export_range(0, 1000, 1) var starting_coins: int = 0
 @export_range(0, 1000, 1) var boarding_enemy_base_reward: int = 1
+@export_range(0, 1000, 1) var strategic_enemy_impact_reward: int = 1
 @export var rewarded_boarding_death_reasons: Array[StringName] = [
 	&"combat",
 	&"anchor_path_closed",
