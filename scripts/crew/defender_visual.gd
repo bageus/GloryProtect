b2de1717e8642534606a715d0d44f833d1d50a5f
bageus <36338626+bageus@users.noreply.github.com>
2026-06-25 15:13:38 +0200

    Integrate turret, captain, and defender animation assets (#138)
    Replace turret visuals by specialization, use directional captain-platform composites, and connect defender idle, run, attack, and death animation frames.

--- scripts/crew/defender_visual.gd
+++ scripts/crew/defender_visual.gd
@@ -1,15 +1,54 @@
 class_name DefenderVisual
 extends Node2D
 
-const REGULAR_TEXTURE: Texture2D = preload(
-	"res://visual/defenders/ChatGPT Image Jun 24, 2026, 12_14_07 AM.png"
-)
-const DRIVER_TEXTURE: Texture2D = preload(
-	"res://visual/defenders/ChatGPT Image Jun 24, 2026, 12_28_49 AM.png"
-)
-const MEDIC_TEXTURE: Texture2D = preload(
-	"res://visual/defenders/ChatGPT Image Jun 24, 2026, 12_29_17 AM.png"
-)
+signal death_animation_finished(defender_id: int)
+
+const WARRIOR_IDLE_TEXTURES: Array[Texture2D] = [
+	preload("res://visual/defenders/warrior/base/idle/warrior_idle_01.png"),
+	preload("res://visual/defenders/warrior/base/idle/warrior_idle_02.png"),
+	preload("res://visual/defenders/warrior/base/idle/warrior_idle_03.png"),
+	preload("res://visual/defenders/warrior/base/idle/warrior_idle_04.png"),
+	preload("res://visual/defenders/warrior/base/idle/warrior_idle_05.png"),
+	preload("res://visual/defenders/warrior/base/idle/warrior_idle_06.png"),
+]
+const WARRIOR_RUN_LEFT_TEXTURES: Array[Texture2D] = [
+	preload("res://visual/defenders/warrior/base/run/left/warrior_run_left_01.png"),
+	preload("res://visual/defenders/warrior/base/run/left/warrior_run_left_02.png"),
+	preload("res://visual/defenders/warrior/base/run/left/warrior_run_left_03.png"),
+	preload("res://visual/defenders/warrior/base/run/left/warrior_run_left_04.png"),
+]
+const WARRIOR_RUN_RIGHT_TEXTURES: Array[Texture2D] = [
+	preload("res://visual/defenders/warrior/base/run/right/warrior_run_right_01.png"),
+	preload("res://visual/defenders/warrior/base/run/right/warrior_run_right_02.png"),
+	preload("res://visual/defenders/warrior/base/run/right/warrior_run_right_03.png"),
+	preload("res://visual/defenders/warrior/base/run/right/warrior_run_right_04.png"),
+]
+const WARRIOR_ATTACK_LEFT_TEXTURES: Array[Texture2D] = [
+	preload("res://visual/defenders/warrior/base/attack/left/warrior_attack_left_01.png"),
+	preload("res://visual/defenders/warrior/base/attack/left/warrior_attack_left_02.png"),
+	preload("res://visual/defenders/warrior/base/attack/left/warrior_attack_left_03.png"),
+	preload("res://visual/defenders/warrior/base/attack/left/warrior_attack_left_04.png"),
+	preload("res://visual/defenders/warrior/base/attack/left/warrior_attack_left_05.png"),
+]
+const WARRIOR_ATTACK_RIGHT_TEXTURES: Array[Texture2D] = [
+	preload("res://visual/defenders/warrior/base/attack/right/warrior_attack_right_01.png"),
+	preload("res://visual/defenders/warrior/base/attack/right/warrior_attack_right_02.png"),
+	preload("res://visual/defenders/warrior/base/attack/right/warrior_attack_right_03.png"),
+	preload("res://visual/defenders/warrior/base/attack/right/warrior_attack_right_04.png"),
+	preload("res://visual/defenders/warrior/base/attack/right/warrior_attack_right_05.png"),
+]
+const WARRIOR_DIE_RIGHT_TEXTURES: Array[Texture2D] = [
+	preload("res://visual/defenders/die/warrior_die_right_01.png"),
+	preload("res://visual/defenders/die/warrior_die_right_02.png"),
+]
+
+enum AnimationState {
+	IDLE,
+	RUN,
+	ATTACK,
+	DYING,
+	HIDDEN,
+}
 
 @export_node_path("HealthComponent") var health_path: NodePath
 @export_node_path("StatusEffectComponent") var status_effects_path: NodePath
@@ -23,15 +62,31 @@ const MEDIC_TEXTURE: Texture2D = preload(
 @export_range(32.0, 128.0, 1.0) var asset_max_width: float = 72.0
 @export_range(0.0, 1.0, 0.01) var alpha_crop_threshold: float = 0.08
 @export var asset_offset: Vector2 = Vector2.ZERO
+@export_group("Animation")
+@export_range(1.0, 30.0, 0.5) var idle_frame_rate: float = 6.0
+@export_range(1.0, 30.0, 0.5) var run_frame_rate: float = 10.0
+@export_range(1.0, 30.0, 0.5) var attack_frame_rate: float = 12.0
+@export_range(1.0, 30.0, 0.5) var death_frame_rate: float = 6.0
+
+static var _alpha_bounds_cache: Dictionary = {}
 
 var _selected: bool = false
 var _poisoned: bool = false
 var _poison_stacks: int = 0
 var _role_id: int = CrewRole.Id.FREE_FIGHTER
-var _regular_source_rect: Rect2
-var _driver_source_rect: Rect2
-var _medic_source_rect: Rect2
+var _state: AnimationState = AnimationState.IDLE
+var _frame_index: int = 0
+var _frame_elapsed: float = 0.0
+var _facing_right: bool = true
+var _idle_source_rects: Array[Rect2] = []
+var _run_left_source_rects: Array[Rect2] = []
+var _run_right_source_rects: Array[Rect2] = []
+var _attack_left_source_rects: Array[Rect2] = []
+var _attack_right_source_rects: Array[Rect2] = []
+var _death_source_rects: Array[Rect2] = []
 var _defender: Defender
+var _movement: DefenderMovement
+var _melee: MeleeAttackComponent
 var _role_manager: CrewRoleManager
 
 @onready var _health: HealthComponent = get_node(health_path)
@@ -39,25 +94,49 @@ var _role_manager: CrewRoleManager
 
 
 func _ready() -> void:
-	_regular_source_rect = TextureRegionLayout.get_alpha_bounds(
-		REGULAR_TEXTURE,
-		alpha_crop_threshold
-	)
-	_driver_source_rect = TextureRegionLayout.get_alpha_bounds(
-		DRIVER_TEXTURE,
-		alpha_crop_threshold
-	)
-	_medic_source_rect = TextureRegionLayout.get_alpha_bounds(
-		MEDIC_TEXTURE,
-		alpha_crop_threshold
-	)
+	_idle_source_rects = _build_source_rects(WARRIOR_IDLE_TEXTURES)
+	_run_left_source_rects = _build_source_rects(WARRIOR_RUN_LEFT_TEXTURES)
+	_run_right_source_rects = _build_source_rects(WARRIOR_RUN_RIGHT_TEXTURES)
+	_attack_left_source_rects = _build_source_rects(WARRIOR_ATTACK_LEFT_TEXTURES)
+	_attack_right_source_rects = _build_source_rects(WARRIOR_ATTACK_RIGHT_TEXTURES)
+	_death_source_rects = _build_source_rects(WARRIOR_DIE_RIGHT_TEXTURES)
 	_defender = get_parent() as Defender
+	_movement = get_node("../DefenderMovement") as DefenderMovement
+	_melee = get_node("../MeleeAttackComponent") as MeleeAttackComponent
 	_role_manager = get_node_or_null(role_manager_path) as CrewRoleManager
 	if _role_manager != null:
 		_role_manager.assignment_changed.connect(_on_assignment_changed)
 		_sync_role_from_manager()
+	if _defender != null:
+		_defender.died.connect(_on_defender_died)
+	if _melee != null:
+		_melee.attack_started.connect(_on_attack_started)
+		_melee.attack_finished.connect(_on_attack_finished)
 	_health.health_changed.connect(_on_health_changed)
 	_status_effects.poison_changed.connect(_on_poison_changed)
+	queue_redraw()
+
+
+func _process(delta: float) -> void:
+	if _state == AnimationState.HIDDEN:
+		return
+	if _state == AnimationState.DYING:
+		_advance_death(delta)
+	elif _state == AnimationState.ATTACK:
+		if _melee == null or not _melee.is_attacking():
+			_sync_locomotion_state()
+		else:
+			_advance_clamped(
+				WARRIOR_ATTACK_RIGHT_TEXTURES.size(),
+				attack_frame_rate,
+				delta
+			)
+	else:
+		_sync_locomotion_state()
+		if _state == AnimationState.RUN:
+			_advance_loop(WARRIOR_RUN_RIGHT_TEXTURES.size(), run_frame_rate, delta)
+		else:
+			_advance_loop(WARRIOR_IDLE_TEXTURES.size(), idle_frame_rate, delta)
 	queue_redraw()
 
 
@@ -86,12 +165,42 @@ func is_selected() -> bool:
 	return _selected
 
 
+func play_death() -> void:
+	if _state == AnimationState.DYING or _state == AnimationState.HIDDEN:
+		return
+	_state = AnimationState.DYING
+	_frame_index = 0
+	_frame_elapsed = 0.0
+	visible = true
+	if _defender != null:
+		_defender.visible = true
+	queue_redraw()
+
+
+func get_animation_state() -> int:
+	return _state
+
+
+func get_animation_frame() -> int:
+	return _frame_index
+
+
+func is_facing_right() -> bool:
+	return _facing_right
+
+
 func _draw() -> void:
+	if _state == AnimationState.HIDDEN:
+		return
 	var source_rect: Rect2 = _get_current_source_rect()
 	var asset_size: Vector2 = _fit_asset_size(source_rect.size)
 	var feet := Vector2(0.0, body_radius) + asset_offset
+	var asset_rect := Rect2(
+		feet - Vector2(asset_size.x * 0.5, asset_size.y),
+		asset_size
+	)
 
-	if _selected:
+	if _state != AnimationState.DYING and _selected:
 		var selection_radius: float = maxf(body_radius + 6.0, asset_size.x * 0.34)
 		draw_arc(
 			feet + Vector2(0.0, -3.0),
@@ -108,43 +217,150 @@ func _draw() -> void:
 			Color(1.0, 0.84, 0.25)
 		)
 
-	var tint := Color.WHITE
-	if _poisoned:
-		tint = Color(0.72, 1.0, 0.62, 1.0)
-	var asset_rect := Rect2(
-		feet - Vector2(asset_size.x * 0.5, asset_size.y),
-		asset_size
-	)
-	draw_texture_rect_region(
-		_get_current_texture(),
-		asset_rect,
-		source_rect,
-		tint
-	)
+	var texture: Texture2D = _get_current_texture()
+	if texture != null:
+		var tint := Color.WHITE
+		if _poisoned and _state != AnimationState.DYING:
+			tint = Color(0.72, 1.0, 0.62, 1.0)
+		draw_texture_rect_region(texture, asset_rect, source_rect, tint)
 
+	if _state == AnimationState.DYING:
+		return
 	if _poisoned:
 		_draw_poison_indicator(asset_rect)
 	_draw_health_segments(asset_rect)
 
 
+func _build_source_rects(textures: Array[Texture2D]) -> Array[Rect2]:
+	var result: Array[Rect2] = []
+	for texture: Texture2D in textures:
+		result.append(_get_cached_alpha_bounds(texture))
+	return result
+
+
+func _get_cached_alpha_bounds(texture: Texture2D) -> Rect2:
+	var key := "%s|%.3f" % [texture.resource_path, alpha_crop_threshold]
+	if _alpha_bounds_cache.has(key):
+		var cached_bounds: Rect2 = _alpha_bounds_cache[key]
+		return cached_bounds
+	var bounds := TextureRegionLayout.get_alpha_bounds(
+		texture,
+		alpha_crop_threshold
+	)
+	_alpha_bounds_cache[key] = bounds
+	return bounds
+
+
+func _sync_locomotion_state() -> void:
+	var moving: bool = (
+		_movement != null
+		and _movement.is_moving()
+		and not _movement.is_paused()
+	)
+	if moving:
+		var delta_x: float = _movement.get_target_x() - _defender.position.x
+		if absf(delta_x) > 0.01:
+			_facing_right = delta_x > 0.0
+		_set_animation_state(AnimationState.RUN)
+	else:
+		_set_animation_state(AnimationState.IDLE)
+
+
+func _set_animation_state(new_state: AnimationState) -> void:
+	if _state == new_state:
+		return
+	_state = new_state
+	_frame_index = 0
+	_frame_elapsed = 0.0
+
+
+func _advance_loop(frame_count: int, frame_rate: float, delta: float) -> void:
+	if frame_count <= 1 or frame_rate <= 0.0:
+		return
+	var frame_duration: float = 1.0 / frame_rate
+	_frame_elapsed += maxf(0.0, delta)
+	while _frame_elapsed >= frame_duration:
+		_frame_elapsed -= frame_duration
+		_frame_index = (_frame_index + 1) % frame_count
+
+
+func _advance_clamped(frame_count: int, frame_rate: float, delta: float) -> void:
+	if frame_count <= 1 or frame_rate <= 0.0 or _frame_index >= frame_count - 1:
+		return
+	var frame_duration: float = 1.0 / frame_rate
+	_frame_elapsed += maxf(0.0, delta)
+	while _frame_elapsed >= frame_duration and _frame_index < frame_count - 1:
+		_frame_elapsed -= frame_duration
+		_frame_index += 1
+
+
+func _advance_death(delta: float) -> void:
+	if death_frame_rate <= 0.0:
+		_finish_death_animation()
+		return
+	var frame_duration: float = 1.0 / death_frame_rate
+	_frame_elapsed += maxf(0.0, delta)
+	while _frame_elapsed >= frame_duration:
+		_frame_elapsed -= frame_duration
+		if _frame_index < WARRIOR_DIE_RIGHT_TEXTURES.size() - 1:
+			_frame_index += 1
+		else:
+			_finish_death_animation()
+			return
+
+
+func _finish_death_animation() -> void:
+	_state = AnimationState.HIDDEN
+	visible = false
+	if _defender != null:
+		_defender.visible = false
+	death_animation_finished.emit(_defender.defender_id if _defender != null else -1)
+
+
 func _get_current_texture() -> Texture2D:
-	match _role_id:
-		CrewRole.Id.DRIVER:
-			return DRIVER_TEXTURE
-		CrewRole.Id.MEDIC:
-			return MEDIC_TEXTURE
-		_:
-			return REGULAR_TEXTURE
+	if _state == AnimationState.DYING:
+		return WARRIOR_DIE_RIGHT_TEXTURES[
+			mini(_frame_index, WARRIOR_DIE_RIGHT_TEXTURES.size() - 1)
+		]
+	if _role_id == CrewRole.Id.DRIVER:
+		return null
+	if _state == AnimationState.ATTACK:
+		var attacks: Array[Texture2D] = (
+			WARRIOR_ATTACK_RIGHT_TEXTURES
+			if _facing_right
+			else WARRIOR_ATTACK_LEFT_TEXTURES
+		)
+		return attacks[mini(_frame_index, attacks.size() - 1)]
+	if _state == AnimationState.RUN:
+		var runs: Array[Texture2D] = (
+			WARRIOR_RUN_RIGHT_TEXTURES
+			if _facing_right
+			else WARRIOR_RUN_LEFT_TEXTURES
+		)
+		return runs[mini(_frame_index, runs.size() - 1)]
+	return WARRIOR_IDLE_TEXTURES[mini(_frame_index, WARRIOR_IDLE_TEXTURES.size() - 1)]
 
 
 func _get_current_source_rect() -> Rect2:
-	match _role_id:
-		CrewRole.Id.DRIVER:
-			return _driver_source_rect
-		CrewRole.Id.MEDIC:
-			return _medic_source_rect
-		_:
-			return _regular_source_rect
+	if _state == AnimationState.DYING:
+		return _death_source_rects[mini(_frame_index, _death_source_rects.size() - 1)]
+	if _role_id == CrewRole.Id.DRIVER:
+		return Rect2(Vector2.ZERO, Vector2(asset_max_width, asset_height))
+	if _state == AnimationState.ATTACK:
+		var attacks: Array[Rect2] = (
+			_attack_right_source_rects
+			if _facing_right
+			else _attack_left_source_rects
+		)
+		return attacks[mini(_frame_index, attacks.size() - 1)]
+	if _state == AnimationState.RUN:
+		var runs: Array[Rect2] = (
+			_run_right_source_rects
+			if _facing_right
+			else _run_left_source_rects
+		)
+		return runs[mini(_frame_index, runs.size() - 1)]
+	return _idle_source_rects[mini(_frame_index, _idle_source_rects.size() - 1)]
 
 
 func _fit_asset_size(source_size: Vector2) -> Vector2:
@@ -178,7 +394,7 @@ func _draw_health_segments(asset_rect: Rect2) -> void:
 		float(_health.max_health) * segment_width
 		+ float(_health.max_health - 1) * gap
 	)
-	var start_x := -total_width * 0.5
+	var start_x := asset_rect.get_center().x - total_width * 0.5
 	var y := asset_rect.position.y - 8.0
 
 	for index: int in range(_health.max_health):
@@ -207,11 +423,34 @@ func _on_assignment_changed(
 	defender_id: int,
 	current_role: int,
 	_target_role: int,
-	_state: int
+	_state_id: int
 ) -> void:
 	if _defender == null or defender_id != _defender.defender_id:
 		return
 	set_role(current_role)
+
+
+func _on_attack_started(target: HealthComponent) -> void:
+	if _state == AnimationState.DYING or _state == AnimationState.HIDDEN:
+		return
+	var target_actor: Node2D = target.get_parent() as Node2D
+	if target_actor != null and _defender != null:
+		_facing_right = target_actor.global_position.x >= _defender.global_position.x
+	_state = AnimationState.ATTACK
+	_frame_index = 0
+	_frame_elapsed = 0.0
+	queue_redraw()
+
+
+func _on_attack_finished() -> void:
+	if _state != AnimationState.ATTACK:
+		return
+	_sync_locomotion_state()
+	queue_redraw()
+
+
+func _on_defender_died(_defender_id: int) -> void:
+	play_death()
 
 
 func _on_health_changed(_current_health: int, _max_health: int) -> void:
