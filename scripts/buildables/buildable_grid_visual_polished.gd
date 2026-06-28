c3f565abac6f46984d84233824ad3007278d08d8
bageus <36338626+bageus@users.noreply.github.com>
2026-06-28 11:10:39 +0200

    Scale turret visual node reliably

--- scripts/buildables/buildable_grid_visual_polished.gd
+++ scripts/buildables/buildable_grid_visual_polished.gd
@@ -1,6 +1,8 @@
 class_name BuildableGridVisualPolished
 extends BuildableGridVisual
 
+const BASE_TURRET_ASSET_SCALE: float = 0.24
+
 @export_range(0.05, 0.5, 0.01) var turret_asset_scale: float = 0.19
 
 
@@ -18,7 +20,6 @@ func _process(delta: float) -> void:
 func _create_turret_visual() -> void:
 	var turret_visual := TurretVisualController.new()
 	turret_visual.name = "TurretVisualController"
-	turret_visual.turret_asset_scale = turret_asset_scale
 	turret_visual.configure(
 		_platform,
 		_grid,
@@ -28,15 +29,25 @@ func _create_turret_visual() -> void:
 		balance
 	)
 	add_child(turret_visual)
+	_apply_turret_scale_to(turret_visual)
+
+
+func get_turret_visual_scale_multiplier() -> float:
+	return turret_asset_scale / BASE_TURRET_ASSET_SCALE
 
 
 func _apply_turret_scale() -> void:
 	var turret_visual := get_node_or_null(
 		"TurretVisualController"
 	) as TurretVisualController
-	if turret_visual == null:
+	if turret_visual != null:
+		_apply_turret_scale_to(turret_visual)
+
+
+func _apply_turret_scale_to(turret_visual: TurretVisualController) -> void:
+	var multiplier: float = get_turret_visual_scale_multiplier()
+	var target_scale := Vector2(multiplier, multiplier)
+	if turret_visual.scale.is_equal_approx(target_scale):
 		return
-	if is_equal_approx(turret_visual.turret_asset_scale, turret_asset_scale):
-		return
-	turret_visual.turret_asset_scale = turret_asset_scale
+	turret_visual.scale = target_scale
 	turret_visual.queue_redraw()
