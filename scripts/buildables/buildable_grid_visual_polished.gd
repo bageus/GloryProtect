3dad724772dcdd67d983a8b3dc88c329532b5a45
bageus <36338626+bageus@users.noreply.github.com>
2026-06-28 11:29:37 +0200

    Use dedicated reduced turret controller

--- scripts/buildables/buildable_grid_visual_polished.gd
+++ scripts/buildables/buildable_grid_visual_polished.gd
@@ -1,25 +1,24 @@
 class_name BuildableGridVisualPolished
 extends BuildableGridVisual
 
-const BASE_TURRET_ASSET_SCALE: float = 0.24
-
 @export_range(0.05, 0.5, 0.01) var turret_asset_scale: float = 0.19
 
 
 func _ready() -> void:
 	super._ready()
-	_apply_turret_scale()
-	call_deferred("_apply_turret_scale")
+	_apply_turret_asset_scale()
+	call_deferred("_apply_turret_asset_scale")
 
 
 func _process(delta: float) -> void:
-	_apply_turret_scale()
+	_apply_turret_asset_scale()
 	super._process(delta)
 
 
 func _create_turret_visual() -> void:
-	var turret_visual := TurretVisualController.new()
+	var turret_visual := TurretVisualControllerPolished.new()
 	turret_visual.name = "TurretVisualController"
+	turret_visual.polished_asset_scale = turret_asset_scale
 	turret_visual.configure(
 		_platform,
 		_grid,
@@ -29,25 +28,12 @@ func _create_turret_visual() -> void:
 		balance
 	)
 	add_child(turret_visual)
-	_apply_turret_scale_to(turret_visual)
 
 
-func get_turret_visual_scale_multiplier() -> float:
-	return turret_asset_scale / BASE_TURRET_ASSET_SCALE
-
-
-func _apply_turret_scale() -> void:
+func _apply_turret_asset_scale() -> void:
 	var turret_visual := get_node_or_null(
 		"TurretVisualController"
-	) as TurretVisualController
-	if turret_visual != null:
-		_apply_turret_scale_to(turret_visual)
-
-
-func _apply_turret_scale_to(turret_visual: TurretVisualController) -> void:
-	var multiplier: float = get_turret_visual_scale_multiplier()
-	var target_scale := Vector2(multiplier, multiplier)
-	if turret_visual.scale.is_equal_approx(target_scale):
+	) as TurretVisualControllerPolished
+	if turret_visual == null:
 		return
-	turret_visual.scale = target_scale
-	turret_visual.queue_redraw()
+	turret_visual.polished_asset_scale = turret_asset_scale
