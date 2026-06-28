2309704e69234eca2c9a603527a1f803becb064a
bageus <36338626+bageus@users.noreply.github.com>
2026-06-28 10:53:08 +0200

    Apply turret scale after visual creation

--- scripts/buildables/buildable_grid_visual_polished.gd
+++ scripts/buildables/buildable_grid_visual_polished.gd
@@ -4,6 +4,16 @@ extends BuildableGridVisual
 @export_range(0.05, 0.5, 0.01) var turret_asset_scale: float = 0.19
 
 
+func _ready() -> void:
+	super._ready()
+	var turret_visual := get_node_or_null(
+		"TurretVisualController"
+	) as TurretVisualController
+	if turret_visual != null:
+		turret_visual.turret_asset_scale = turret_asset_scale
+		turret_visual.queue_redraw()
+
+
 func _create_turret_visual() -> void:
 	var turret_visual := TurretVisualController.new()
 	turret_visual.name = "TurretVisualController"
