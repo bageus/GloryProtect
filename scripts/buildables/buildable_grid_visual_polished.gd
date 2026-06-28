a1c229b8b1b71f82cd2fedd6c4d59ebdbcc50654
bageus <36338626+bageus@users.noreply.github.com>
2026-06-28 11:48:43 +0200

    Use constant-scale turret visual

--- scripts/buildables/buildable_grid_visual_polished.gd
+++ scripts/buildables/buildable_grid_visual_polished.gd
@@ -4,21 +4,9 @@ extends BuildableGridVisual
 @export_range(0.05, 0.5, 0.01) var turret_asset_scale: float = 0.19
 
 
-func _ready() -> void:
-	super._ready()
-	_apply_turret_asset_scale()
-	call_deferred("_apply_turret_asset_scale")
-
-
-func _process(delta: float) -> void:
-	_apply_turret_asset_scale()
-	super._process(delta)
-
-
 func _create_turret_visual() -> void:
 	var turret_visual := TurretVisualControllerPolished.new()
 	turret_visual.name = "TurretVisualController"
-	turret_visual.polished_asset_scale = turret_asset_scale
 	turret_visual.configure(
 		_platform,
 		_grid,
@@ -28,12 +16,3 @@ func _create_turret_visual() -> void:
 		balance
 	)
 	add_child(turret_visual)
-
-
-func _apply_turret_asset_scale() -> void:
-	var turret_visual := get_node_or_null(
-		"TurretVisualController"
-	) as TurretVisualControllerPolished
-	if turret_visual == null:
-		return
-	turret_visual.polished_asset_scale = turret_asset_scale
