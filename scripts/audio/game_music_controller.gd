738f241574ee4b21dd5efda9cbd269d1f856ac61
bageus <36338626+bageus@users.noreply.github.com>
2026-07-03 11:46:25 +0200

    Use new music tracks in gameplay playlist

--- scripts/audio/game_music_controller.gd
+++ scripts/audio/game_music_controller.gd
@@ -3,8 +3,17 @@ extends Node
 
 signal music_track_changed(track_id: StringName)
 
-const GAMEPLAY_STREAM: AudioStream = preload(
+const GAMEPLAY_STREAM_1: AudioStream = preload(
 	"res://audio/Melodic Drive Core.mp3"
+)
+const GAMEPLAY_STREAM_2: AudioStream = preload(
+	"res://audio/Melodic Drive Core2.mp3"
+)
+const GAMEPLAY_STREAM_3: AudioStream = preload(
+	"res://audio/Melodic Drive Core3.mp3"
+)
+const GAMEPLAY_STREAM_4: AudioStream = preload(
+	"res://audio/Melodic core.mp3"
 )
 const GAME_OVER_STREAM: AudioStream = preload(
 	"res://audio/game_over.ogg"
@@ -21,8 +30,15 @@ const TRACK_GAME_OVER: StringName = &"game_over"
 @export_range(-40.0, 6.0, 0.5) var game_over_volume_db: float = -6.0
 
 var _active_track: StringName = TRACK_NONE
+var _gameplay_track_index: int = 0
 var _gameplay_player: AudioStreamPlayer
 var _game_over_player: AudioStreamPlayer
+var _gameplay_streams: Array[AudioStream] = [
+	GAMEPLAY_STREAM_1,
+	GAMEPLAY_STREAM_2,
+	GAMEPLAY_STREAM_3,
+	GAMEPLAY_STREAM_4,
+]
 
 @onready var _game_flow: GameFlowController = get_node(game_flow_path)
 
@@ -31,12 +47,13 @@ func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	_gameplay_player = _create_music_player(
 		"GameplayMusic",
-		GAMEPLAY_STREAM,
+		null,
 		gameplay_volume_db
 	)
+	_gameplay_player.finished.connect(_on_gameplay_track_finished)
 	_game_over_player = _create_music_player(
 		"GameOverMusic",
-		GAME_OVER_STREAM,
+		_make_loop_stream(GAME_OVER_STREAM),
 		game_over_volume_db
 	)
 	if not _game_flow.run_state_changed.is_connected(_on_run_state_changed):
@@ -56,8 +73,27 @@ func is_game_over_music_active() -> bool:
 	return _active_track == TRACK_GAME_OVER
 
 
+func get_gameplay_track_count() -> int:
+	return _gameplay_streams.size()
+
+
+func get_current_gameplay_track_index() -> int:
+	return _gameplay_track_index
+
+
+func get_gameplay_track_paths_for_tests() -> PackedStringArray:
+	var paths := PackedStringArray()
+	for stream: AudioStream in _gameplay_streams:
+		paths.append(stream.resource_path)
+	return paths
+
+
 func refresh_music_state_for_tests() -> void:
 	_sync_to_run_state(_game_flow.state)
+
+
+func advance_gameplay_track_for_tests() -> void:
+	_on_gameplay_track_finished()
 
 
 func _create_music_player(
@@ -68,7 +104,7 @@ func _create_music_player(
 	var player := AudioStreamPlayer.new()
 	player.name = player_name
 	player.process_mode = Node.PROCESS_MODE_ALWAYS
-	player.stream = _make_loop_stream(source)
+	player.stream = source
 	player.bus = String(AppSettingsService.MUSIC_BUS)
 	player.volume_db = volume_db
 	add_child(player)
@@ -81,6 +117,15 @@ func _make_loop_stream(source: AudioStream) -> AudioStream:
 		(result as AudioStreamMP3).loop = true
 	elif result is AudioStreamOggVorbis:
 		(result as AudioStreamOggVorbis).loop = true
+	return result
+
+
+func _make_play_once_stream(source: AudioStream) -> AudioStream:
+	var result: AudioStream = source.duplicate(true) as AudioStream
+	if result is AudioStreamMP3:
+		(result as AudioStreamMP3).loop = false
+	elif result is AudioStreamOggVorbis:
+		(result as AudioStreamOggVorbis).loop = false
 	return result
 
 
@@ -102,12 +147,29 @@ func _set_active_track(track_id: StringName) -> void:
 	_active_track = track_id
 	match track_id:
 		TRACK_GAMEPLAY:
-			_gameplay_player.volume_db = gameplay_volume_db
-			_gameplay_player.play()
+			_play_gameplay_track(_gameplay_track_index)
 		TRACK_GAME_OVER:
 			_game_over_player.volume_db = game_over_volume_db
 			_game_over_player.play()
 	music_track_changed.emit(track_id)
+
+
+func _play_gameplay_track(track_index: int) -> void:
+	if _gameplay_streams.is_empty():
+		return
+	_gameplay_track_index = wrapi(track_index, 0, _gameplay_streams.size())
+	_gameplay_player.stop()
+	_gameplay_player.stream = _make_play_once_stream(
+		_gameplay_streams[_gameplay_track_index]
+	)
+	_gameplay_player.volume_db = gameplay_volume_db
+	_gameplay_player.play()
+
+
+func _on_gameplay_track_finished() -> void:
+	if _active_track != TRACK_GAMEPLAY:
+		return
+	_play_gameplay_track(_gameplay_track_index + 1)
 
 
 func _on_run_state_changed(_previous_state: int, new_state: int) -> void:
